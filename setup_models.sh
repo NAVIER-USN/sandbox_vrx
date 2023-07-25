24d268a11a822547393765f87d0691610f8eae02
edvart-ros <240818@usn.no>
2023-07-25 10:15:30 +0000

    modify for local dev container

--- setup_models.sh
+++ setup_models.sh
@@ -1,16 +1,16 @@
-echo "/usr/share/gazebo-11/setup.sh" >> ~/.bashrc
-echo "source /opt/overlay_ws/src/sandbox/vrx_navier/devel/setup.bash" >> ~/.bashrc
+#echo "/usr/share/gazebo-11/setup.sh" >> ~/.bashrc
+#echo "source /workspaces/sandbox_vrx/vrx_navier/devel/setup.bash" >> ~/.bashrc
 
 echo copying models to gzweb directory
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/vrx_gazebo /opt/gzweb/http/client/assets/
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/vrx_gazebo/models/* /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/vrx_gazebo /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/vrx_gazebo/models/* /opt/gzweb/http/client/assets/
 
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/wamv_description/models/* /opt/gzweb/http/client/assets/
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/wamv_description /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/wamv_description/models/* /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/wamv_description /opt/gzweb/http/client/assets/
 
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/wamv_gazebo/models/gps /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/wamv_gazebo/models/gps /opt/gzweb/http/client/assets/
 
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/wave_gazebo/world_models/ocean_waves /opt/gzweb/http/client/assets/
-cp -r /opt/overlay_ws/src/sandbox/vrx_navier/src/vrx/wave_gazebo /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/wave_gazebo/world_models/ocean_waves /opt/gzweb/http/client/assets/
+cp -r /workspaces/sandbox_vrx/vrx_navier/src/vrx/wave_gazebo /opt/gzweb/http/client/assets/
 
 echo models copied
